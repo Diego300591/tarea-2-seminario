89d27835c5c93047333fe2d22eba002795e13e64
Diego <diegocastanares3005@gmail.com>
2014-11-09 18:38:22 -0400

    Ajustes finales

--- Tienda/apps/Productos/forms.py
+++ Tienda/apps/Productos/forms.py
@@ -4,7 +4,9 @@ from .models import *
 class fproducto(ModelForm):
 	class Meta:
 		model=Producto
+		fields=("Nombre","Descripcion","Precio","Stock")
 class fpedido(ModelForm):
 	lista_producto=forms.ModelMultipleChoiceField(queryset=Producto.objects.all(),widget=forms.CheckboxSelectMultiple())
 	class Meta:
 		model=Pedido 
+		exclude=["Fecha_compra"]
